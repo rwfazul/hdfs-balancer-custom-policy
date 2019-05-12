fd02d506cb1faed638fff2eaa9eca97097257ff0
rwfazul <rwfazul@inf.ufsm.br>
2019-05-12 01:36:36 -0300

    rack fault tolerant policy

--- scripts/deploy/commit_changes.sh
+++ scripts/deploy/commit_changes.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-str=$*
-
 git status
 
 echo -e "\n"
@@ -11,8 +9,8 @@ if [ "${opt,,}" == "y" ] || [ "${opt,,}" == "yes" ]; then
 	read -p "Type the commit message: `echo $'\n> '`" message
 	git pull origin master
 	git add .
-	# git commit -m "$message"
-	#git push -u origin master
+	git commit -m "$message"
+	git push origin $(git branch | grep \* | cut -d ' ' -f2)
 else
 	echo -e "\n--> Noting was commited!!"
 fi
