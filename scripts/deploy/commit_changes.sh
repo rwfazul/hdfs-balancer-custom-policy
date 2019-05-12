a31664c15635d95d337337e1b4dfbeaaa9a61daf
rwfazul <rwfazul@inf.ufsm.br>
2019-05-12 02:18:22 -0300

    script co commit git changes

--- scripts/deploy/commit_changes.sh
+++ scripts/deploy/commit_changes.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-ARGC=$# 
-MAX_ARGS=1
-BRANCH=$1
+git status
 
-if [ $ARGC -ne $MAX_ARGS ]; then 
-	echo -e "Usage: $0 <branch_name>\n">&2 
-	exit 1
+echo -e "\n"
+read -p  "---> Do you want commit your changes (y/n)? " opt
+
+if [ "${opt,,}" == "y" ] || [ "${opt,,}" == "yes" ]; then
+	read -p "Type the commit message: `echo $'\n> '`" message
+	git pull origin master
+	git add .
+	git commit -m "$message"
+	git push origin $(git branch | grep \* | cut -d ' ' -f2)
+else
+	echo -e "\n--> Noting was commited!!"
 fi
-
-# git fetch --all
-git pull --all
-git checkout $BRANCH
