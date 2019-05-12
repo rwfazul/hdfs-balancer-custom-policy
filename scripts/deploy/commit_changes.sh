691662486440e903160e4f4c3ea12762ce5827c5
rwfazul <rwfazul@inf.ufsm.br>
2019-05-12 01:31:32 -0300

    test rackfaulttolerantpolicy

---
+++ scripts/deploy/commit_changes.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+str=$*
+
+git status
+
+echo -e "\n"
+read -p  "---> Do you want commit your changes (y/n)? " opt
+
+if [ "${opt,,}" == "y" ] || [ "${opt,,}" == "yes" ]; then
+	read -p "Type the commit message: `echo $'\n> '`" message
+	git pull
+	git add .
+	git commit -m "$message"
+	#git push -u origin master
+else
+	echo -e "\n--> Noting was commited!!"
+fi
