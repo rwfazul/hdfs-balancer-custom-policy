0fdd57eefe82d35f4f65837fc589a4e61460ab6a
rwfazul <rwfazul@inf.ufsm.br>
2019-05-16 15:31:31 -0300

    fix path issue

--- scripts/deploy/grid5000_copy_jars.sh
+++ scripts/deploy/grid5000_copy_jars.sh
@@ -3,8 +3,8 @@
 HADOOP="$HOME/hadoop/hadoop"
 JAR_DEST_PATH=../../target
 
-cp $JAR_DEST_PATH/target/hadoop-hdfs-2.9.2-sources.jar $HADOOP/share/hadoop/hdfs/sources/hadoop-hdfs-2.9.2-sources.jar
-cp $JAR_DEST_PATH/target/hadoop-hdfs-2.9.2-test-sources.jar $HADOOP/share/hadoop/hdfs/sources/hadoop-hdfs-2.9.2-test-sources.jar
-cp $JAR_DEST_PATH/target/hadoop-hdfs-2.9.2-tests.jar $HADOOP/share/hadoop/hdfs/hadoop-hdfs-2.9.2-tests.jar
-cp $JAR_DEST_PATH/target/hadoop-hdfs-2.9.2.jar $HADOOP/share/hadoop/hdfs/hadoop-hdfs-2.9.2.jar
-cp $JAR_DEST_PATH/target/hadoop-hdfs-2.9.2.jar $HADOOP/share/hadoop/httpfs/tomcat/webapps/webhdfs/WEB-INF/lib/hadoop-hdfs-2.9.2.jar
+cp $JAR_DEST_PATH/hadoop-hdfs-2.9.2-sources.jar $HADOOP/share/hadoop/hdfs/sources/hadoop-hdfs-2.9.2-sources.jar
+cp $JAR_DEST_PATH/hadoop-hdfs-2.9.2-test-sources.jar $HADOOP/share/hadoop/hdfs/sources/hadoop-hdfs-2.9.2-test-sources.jar
+cp $JAR_DEST_PATH/hadoop-hdfs-2.9.2-tests.jar $HADOOP/share/hadoop/hdfs/hadoop-hdfs-2.9.2-tests.jar
+cp $JAR_DEST_PATH/hadoop-hdfs-2.9.2.jar $HADOOP/share/hadoop/hdfs/hadoop-hdfs-2.9.2.jar
+cp $JAR_DEST_PATH/hadoop-hdfs-2.9.2.jar $HADOOP/share/hadoop/httpfs/tomcat/webapps/webhdfs/WEB-INF/lib/hadoop-hdfs-2.9.2.jar
