29716561baebe66cdeb71373f797a2b81f06fd93
rwfazul <rwfazul@inf.ufsm.br>
2019-05-17 19:39:01 -0300

    move to and from hadoop balancer directory

---
+++ scripts/deploy/move_from_project.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SOURCE=~/Desktop/hadoop/hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/balancer
+BALANCER_PATH=../../balancer
+
+cp $SOURCE/*.java $BALANCER_PATH
--- scripts/deploy/move_to_project.sh
+++ scripts/deploy/move_to_project.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-SOURCE=../../balancer
-BALANCER_PATH=~/Desktop/hadoop/hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/balancer
+echo "this will overwrite the modifications in HDFS Balancer source code... to use it descomment the lines below"
+# SOURCE=../../balancer
+# BALANCER_PATH=~/Desktop/hadoop/hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/balancer
 
-cp $SOURCE/*.java $BALANCER_PATH
+# cp $SOURCE/*.java $BALANCER_PATH
