1c80fed982580a8f5e6b79e5ffcb4307be37b69b
rwfazul <rwfazul@inf.ufsm.br>
2019-05-30 02:03:40 -0300

    test 05/30

---
+++ target/move.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+scp hadoop-hdfs-2.9.2.jar rfazul@access.grid5000.fr:lyon/hadoop/hdfs-balancer-custom-policy/target/hadoop-hdfs-2.9.2.jar
+scp hadoop-hdfs-2.9.2-tests.jar rfazul@access.grid5000.fr:lyon/hadoop/hdfs-balancer-custom-policy/target/hadoop-hdfs-2.9.2-tests.jar
+scp hadoop-hdfs-2.9.2-sources.jar rfazul@access.grid5000.fr:lyon/hadoop/hdfs-balancer-custom-policy/target/hadoop-hdfs-2.9.2-sources.jar
+scp hadoop-hdfs-2.9.2-test-sources.jar rfazul@access.grid5000.fr:lyon/hadoop/hdfs-balancer-custom-policy/target/hadoop-hdfs-2.9.2-test-sources.jar
